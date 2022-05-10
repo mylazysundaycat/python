bab5e365eae2cbccf0718379113211a0a2dec4c5
mylazysundaycat <mylazysundaycat@gmail.com>
2022-05-10 23:02:09 +0900

    no message

---
+++ 자료형.py
@@ -0,0 +1,26 @@
+
+#숫자형
+a = 1
+print(a)
+print(type(a))
+
+b = 1.23
+print(type(b))
+
+#정수형: int  실수형: float
+
+
+#4/3 = 몫 0 나머지3
+c=3
+d=4
+print(c/d) #나누기
+print(c//d)  #몫
+print(c%d) #나머지
+
+#문자형
+#"" , ''
+a = "hi"
+print(type(a)) # str 은 문자열
+
+b= "yj's calling" # 
+print(b)
